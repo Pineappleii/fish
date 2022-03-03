b910739c57cba3fa808139a608b1c95a40102997
Pineappleii <774153840@qq.com>
2022-03-03 15:01:47 +0800

    【fish】用dotenv获取配置文件

--- _env_settings.py
+++ _env_settings.py
@@ -1,9 +1,12 @@
-from os import environ
+import os
+
+from dotenv import load_dotenv
 
 
 class settings:
-    JMSMFA = environ.get('JMSMFA')
-    ALIMFA = environ.get('ALIMFA')
-    ZF_USERNAME = environ.get('ZFRONTIER_USERNAME')
-    ZF_PASSWORD = environ.get('ZFRONTIER_PASSWORD')
-    ZF_USERID = environ.get('ZFRONTIER_USERID')
+    load_dotenv(verbose=True)
+    JMSMFA = os.getenv('JMSMFA')
+    ALIMFA = os.getenv('ALIMFA')
+    ZF_USERNAME = os.getenv('ZFRONTIER_USERNAME')
+    ZF_PASSWORD = os.getenv('ZFRONTIER_PASSWORD')
+    ZF_USERID = os.getenv('ZFRONTIER_USERID')
